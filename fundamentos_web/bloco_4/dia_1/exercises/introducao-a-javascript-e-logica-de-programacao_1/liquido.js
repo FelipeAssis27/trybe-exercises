5b8e0c6f378a0d7770b49bc431e581235e5e47ae
FelipeAssis27 <assisfelipe.gomes@gmail.com>
2021-02-25 20:29:03 -0300

    exercício da folha salarial corrigido

--- fundamentos_web/bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/liquido.js
+++ fundamentos_web/bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/liquido.js
@@ -1,40 +1,42 @@
-let salBruto = 1500.10;
+let salBruto = 3000;
+let descInss = 0;
+let salLiquido;
 const alq1 = 7.5/100;
 const alq2 = 15/100;
 const alq3 = 22.5/100;
 const alq4 = 27.5/100;
 
 if (salBruto <= 1556.94) {
-    let descInss = salBruto - salBruto *8/100;
+     descInss = salBruto - salBruto *8/100;
 }
-else if (1556.94 < salBruto <= 2594.92) {
-    let descInss = salBruto - salBruto *9/100;
+else if (salBruto >= 1556.95 && salBruto <= 2594.92) {
+    descInss = salBruto - salBruto *9/100;
 }
-else if (2594.92 < salBruto <= 5.189.82) {
-    let descInss = salBruto - salBruto *11/100;
+else if (salBruto >= 2594.93 && salBruto <= 5189.82) {
+     descInss = salBruto - salBruto *11/100;
 }
 else {
-    let descInss = salBruto - 570.88;
+     descInss = salBruto - 570.88;
 }
+console.log(descInss);
 
 if (descInss <= 1903.98) {
-    let Ir = 0;
-    let salLiquido = descInss;
+    salLiquido = descInss;
     console.log(salLiquido);
 }
-else if (1903.98 < descInss <= 2826.65) {
-    let ir = descInss - descInss * alq1 - 142.80;
-    console.log(ir);
+else if (descInss >= 1903.99 && descInss <= 2826.65) {
+    salLiquido = descInss - descInss * alq1 + 142.80;
+    console.log(salLiquido);
 }
-else if (2826.65 < descInss <= 3751.05) {
-    let ir = descInss - descInss * alq2 - 354.80;
-    console.log(ir);
+else if (descInss >= 2826.66 && descInss <= 3751.05) {
+    salLiquido = descInss - descInss * alq2 + 354.80;
+    console.log(salLiquido);
 }
-else if (3751.05 < descInss <= 4664.68) {
-    let ir = descInss - descInss * alq3 - 636.13;
-    console.log(ir);
+else if (descInss >= 3751.06 && descInss <= 4664.68) {
+    salLiquido = descInss - descInss * alq3 + 636.13;
+    console.log(salLiquido);
 }
 else {
-    let ir = descInss - descInss * alq4 - 869.36;
-    console.log(ir);
+    salLiquido = descInss - descInss * alq4 + 869.36;
+    console.log(salLiquido);
 }
