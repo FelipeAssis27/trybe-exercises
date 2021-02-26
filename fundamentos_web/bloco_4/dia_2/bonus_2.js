ddd94996d45ca587ff4067a840764807dae1cf75
FelipeAssis27 <71336221+FelipeAssis27@users.noreply.github.com>
2021-02-26 18:26:18 -0300

    Add files via upload
    exercicios bonus corrigidos.

--- fundamentos_web/bloco_4/dia_2/bonus_2.js
+++ fundamentos_web/bloco_4/dia_2/bonus_2.js
@@ -1,10 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-numbers.sort(ordenacao);
-numbers.reverse();
+for (let i = 1; i < numbers.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (numbers[i] > numbers[j]) {
+      let position = numbers[i];
 
-function ordenacao(a, b) {
-  return a-b;
-  //sem 'numbers.reverse()' -> 'return b - a';  
+      numbers[i] = numbers[j];
+      numbers[j] = position;
+    }
+  }
 }
-  console.log(numbers);
+
+console.log(numbers);
--- fundamentos_web/bloco_4/dia_2/bonus_3.js
+++ fundamentos_web/bloco_4/dia_2/bonus_3.js
@@ -1,9 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let position = 1;
 let array = [];
 
 for (let i = 0; i < numbers.length; i++) {
-    position = position * numbers[i];
-    array.push(position); 
+    if (numbers[i + 1]) {
+        array.push(numbers[i] * numbers[i + 1]);
+    } else {
+        array.push(numbers[i] * 2);
+    }
 }
 console.log(array);
